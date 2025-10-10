50a1967bb85f23675babb5aa4e83d3f0032e4778
ngonza81 <ngonza81@ucsc.edu>
2025-10-10 08:43:18 +0000

    Added automatic counter (Completed Step 3)

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import "./style.css";
 
+// Global Variables
 let counter = 0;
+
+// Counter Function
+function incrementCounter() {
+  counter += 1;
+  counterElement.textContent = counter.toString();
+}
 
 document.body.innerHTML = `
   <button id="Click">Make Cookies! 👆</button>
-  <p>Counter: <span id="counter">0</span></p>
+  <p>Cookies: <span id="counter">0</span></p>
 `;
 
 // Click handler
 const button = document.getElementById("Click")!;
 const counterElement = document.getElementById("counter")!;
 
-// Increment counter
+// Increment counter (By User)
 button.addEventListener("click", () => {
-  counter += 1;
-  counterElement.textContent = counter.toString();
+  incrementCounter();
   console.log("+1!");
 });
+
+// Increment counter (Automatically)
+setInterval(() => {
+  incrementCounter();
+}, 1000);
