cda7665581bdc02f7d7055a509466e21fee71cef
ngonza81 <ngonza81@ucsc.edu>
2025-10-10 16:57:24 +0000

    Added Animation frame counter Removed SetInterval  (Completed Step 4)

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,7 @@ import "./style.css";
 
 // Global Variables
 let counter = 0;
-
-// Counter Function
-function incrementCounter() {
-  counter += 1;
-  counterElement.textContent = counter.toString();
-}
+let lastTime = performance.now();
 
 document.body.innerHTML = `
   <button id="Click">Make Cookies! 👆</button>
@@ -18,13 +13,22 @@ document.body.innerHTML = `
 const button = document.getElementById("Click")!;
 const counterElement = document.getElementById("counter")!;
 
+// Increment counter (Automatically by frame rate)
+function animate(currentTime: number) {
+  const deltaTime = currentTime - lastTime;
+  lastTime = currentTime;
+
+  counter += (1 * deltaTime) / 1000;
+
+  counterElement.textContent = Math.floor(counter).toString();
+
+  requestAnimationFrame(animate);
+}
+
 // Increment counter (By User)
 button.addEventListener("click", () => {
-  incrementCounter();
-  console.log("+1!");
+  counter += 1;
+  counterElement.textContent = counter.toString();
 });
 
-// Increment counter (Automatically)
-setInterval(() => {
-  incrementCounter();
-}, 1000);
+requestAnimationFrame(animate);
